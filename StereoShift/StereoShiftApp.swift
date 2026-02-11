e3cb9a49a22eda05f62ee73b11a7dd415d2f23a6
Ethan <lisen8018@gmail.com>
2026-02-12 08:04:40 +1300

    Initial Commit

---
+++ StereoShift/StereoShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StereoShiftApp.swift
+//  StereoShift
+//
+//  Created by Eason Smith on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct StereoShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StereoShiftTests/StereoShiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  StereoShiftTests.swift
+//  StereoShiftTests
+//
+//  Created by Eason Smith on 2/12/26.
+//
+
+import Testing
+@testable import StereoShift
+
+struct StereoShiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
